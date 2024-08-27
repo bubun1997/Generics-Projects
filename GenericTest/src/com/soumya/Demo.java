39411aec67c10167222a3c0b155859dd685225b3
Soumyajit Banerjee <soumyajit4banerjee@gmail.com>
2024-08-27 20:54:57 +0530

    Add First Demo Project

---
+++ GenericTest/src/com/soumya/Demo.java
@@ -0,0 +1,31 @@
+package com.soumya;
+
+import java.util.List;
+
+interface Test<T>{
+	
+	  List<T> getNames(T...t);
+}
+
+public class Demo {
+
+	static <U> Test<U> getTest(U...obj){
+				
+		System.out.println("get");
+		return (data) -> List.of(obj);
+	}
+	public static void main(String[] args) {
+
+		Test<String> names = getTest("Ram","Kunal","Aman","Roshni","Devika");
+		
+		System.out.println("Hello");
+		
+		List<String> l = names.getNames("John","Michael");
+		
+		System.out.println(l);
+				
+		System.out.println("Terminated...");
+	}
+ 
+	
+}
